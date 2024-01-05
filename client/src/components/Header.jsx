ab47eb4b863eca85a8abba38ca118ddc6be29c3d
Yemi Oduyemi <yhermii@gmail.com>
2024-01-05 23:44:44 +0100

    Navigation popup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,51 +1,52 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import "../style.scss";
+import "../style.scss";
 import sitelogo from "../assets/images/logo/sitelogo.png";
 import sitelogoWhite from "../assets/images/logo/sitelogo_white.png";
 
 
   export const Header = ({ isWhiteBackground = false }) => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState("");
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
+    const toggleMobileMenu = () => {
+      setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
 
-  const renderMobileMenu = () => {
-   
-    return (
-      isMobileMenuOpen && (
-        <div className="md:hidden">
-          <Link to="/" className="text-l block py-2 hover:text-butter">
-            Home
-          </Link>
-          <Link to="/about" className="text-l block py-2 hover:text-butter">
-            About
-          </Link>
-          <Link to="/cv" className="text-l block py-2 hover:text-butter">
-            Curriculum vitae
-          </Link>
-          <Link
-            to="/body-of-works"
-            className="text-l block py-2 hover:text-butter"
-          >
-            Body of Works
-          </Link>
-          <Link to="/contact" className="text-l block py-2 hover:text-butter">
-            Contact
-          </Link>
-          {/* <Link to="/" className="text-l block py-2 hover:text-butter">
-            <img
-              src={isWhiteBackground ? sitelogoWhite : sitelogo}
-              alt="logo"
-              className={isWhiteBackground ? "white-logo" : "black-logo"}
-            />
-          </Link> */}
-        </div>
-      )
-    );
-  };
+    const closeMobileMenu = () => {
+      setIsMobileMenuOpen(false);
+    };
+
+    const renderMobileMenu = () => {
+      return (
+        isMobileMenuOpen && (
+          <div className="mobile-menu-popup">
+            <div className="mobile-menu-content">
+              <button className="close-button font-semibold" onClick={closeMobileMenu}>
+                X
+              </button>
+              <Link to="/" className="mobile-menu-link">
+                Home
+              </Link>
+              <Link to="/about" className="mobile-menu-link">
+                About
+              </Link>
+              <Link to="/cv" className="mobile-menu-link">
+                Curriculum vitae
+              </Link>
+              <Link
+                to="/body-of-works"
+                className="mobile-menu-link"
+              >
+                Body of Works
+              </Link>
+              <Link to="/contact" className="mobile-menu-link">
+                Contact
+              </Link>
+            </div>
+          </div>
+        )
+      );
+    };
   
   return (
     <header className={`bg-transparent main_header`}>
@@ -54,22 +55,19 @@ import sitelogoWhite from "../assets/images/logo/sitelogo_white.png";
         className={`bg-transparent }`}
       >
         <div className="mt-0 py-1 flex items-center nav-icon justify-between">
-          <div className="mobile-menu-button md:hidden">
-            <button className="text-gray-700 p-2" onClick={toggleMobileMenu}>
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 4H4a1 1 0 100 2h8a1 1 0 100-2zM4 10a1 1 0 110-2h8a1 1 0 110 2H4zm8 3a1 1 0 100 2H4a1 1 0 100-2h8z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-            {renderMobileMenu()}
+          <div className="mobile-menu-icon md:hidden" onClick={toggleMobileMenu}>
+            <svg
+              className="w-12 h-12"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12 4H4a1 1 0 100 2h8a1 1 0 100-2zM4 10a1 1 0 110-2h8a1 1 0 110 2H4zm8 3a1 1 0 100 2H4a1 1 0 100-2h8z"
+                clipRule="evenodd"
+              />
+            </svg>
           </div>
           <div className="mx-auto logo">
             <Link to="/" className="text-l block py-2 w-full">
@@ -127,6 +125,7 @@ import sitelogoWhite from "../assets/images/logo/sitelogo_white.png";
           </ul>
         </div>
       </nav>
+      {renderMobileMenu()}
     </header>
   );  
 };
